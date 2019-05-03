6e5fbc70a00379bd7cdc0d1a320be3d8049789a1
b401 <sec@i-401.xyz>
2019-05-03 20:17:10 +0200

    clippy and rustfmt

--- src/floodrs.rs
+++ src/floodrs.rs
@@ -1,42 +1,47 @@
 extern crate image;
 
-use std::net::TcpStream;
-use std::io::Write;
 use image::open;
+use std::io::Write;
+use std::net::TcpStream;
+use std::net::{Ipv4Addr, SocketAddrV4};
 use std::path::Path;
 use std::time::Instant;
-use std::net::{Ipv4Addr,SocketAddrV4};
 
-pub struct Config{
+pub struct Config {
     pub server: SocketAddrV4,
     pub image: image::ImageBuffer<image::Rgba<u8>, std::vec::Vec<u8>>,
-    pub offset: (u32,u32),
+    pub offset: (u32, u32),
     pub rpt: bool,
 }
 
-
 impl Config {
-    pub fn new(ip: &str,port: u16,image: image::RgbaImage,x_offset: u32,y_offset: u32,repeat: bool) -> Config {
+    pub fn new(
+        ip: &str,
+        port: u16,
+        image: image::RgbaImage,
+        x_offset: u32,
+        y_offset: u32,
+        repeat: bool,
+    ) -> Config {
         Config {
-            server : SocketAddrV4::new(
-                        ip.parse().unwrap_or(
-                            Ipv4Addr::new(127,0,0,1)
-                        ),
-                        port),
-            image : image,
+            server: SocketAddrV4::new(
+                ip.parse().unwrap_or_else(|_| Ipv4Addr::new(127, 0, 0, 1)),
+                port,
+            ),
+            image,
             offset: (x_offset, y_offset),
             rpt: repeat,
         }
     }
 }
 
-pub fn run(pixelflut_infos: Config) -> Result<(),std::io::Error> {
+pub fn run(pixelflut_infos: Config) -> Result<(), std::io::Error> {
     let time: u128 = write_pixels(pixelflut_infos)?;
     println!("[*] {} px/s", time);
     Ok(())
 }
 
-pub fn open_image(image_path: &Path) -> Result<image::RgbaImage,image::ImageError> {
+pub fn open_image(image_path: &Path) -> Result<image::RgbaImage, image::ImageError> {
     let image: Result<image::DynamicImage, image::ImageError> = open(image_path);
 
     match image {
@@ -45,39 +50,52 @@ pub fn open_image(image_path: &Path) -> Result<image::RgbaImage,image::ImageErro
     }
 }
 
-fn write_pixels(pixelflut_infos: Config) -> Result<u128,std::io::Error> {
-    let con =  TcpStream::connect(pixelflut_infos.server);
+fn write_pixels(pixelflut_infos: Config) -> Result<u128, std::io::Error> {
+    let con = TcpStream::connect(pixelflut_infos.server);
 
-    let (x,y) = pixelflut_infos.offset;
+    let (x, y) = pixelflut_infos.offset;
     let image_pixels = pixelflut_infos.image;
 
     let mut counter = 0;
     let time = Instant::now();
 
+    // Caching
+    let mut pixel_cache: Vec<Vec<u8>> = Vec::new();
+    for pixel in image_pixels.enumerate_pixels() {
+        pixel_cache.push(
+            format!(
+                "PX {} {} {:0>8}\n",
+                x + pixel.0,
+                y + pixel.1,
+                pixel
+                    .2
+                    .data
+                    .iter()
+                    .map(|s| format!("{:x}", s))
+                    .collect::<String>()
+            )
+            .into_bytes(),
+        );
+    }
+
     match con {
         Ok(mut connection) => {
             loop {
-                for pixel in image_pixels.enumerate_pixels() {
-                    match connection.write(&format!("PX {} {} {:0>8}\n",
-                                    x + pixel.0,
-                                    y + pixel.1,
-                                    pixel.2.data.iter().map(|s| format!("{:x}",s)).collect::<String>()).as_bytes()){
+                for pixel in &pixel_cache {
+                    match connection.write(&pixel) {
                         Ok(_) => counter += 1,
                         Err(e) => {
-                            println!("[*] Err: {}",e);
-                        },
+                            println!("[*] Err: {}", e);
+                        }
                     }
-                };
-                if !pixelflut_infos.rpt {
-                    break
                 }
-            };
 
-            Ok(60*counter/(time.elapsed().as_millis()/100))
+                if !pixelflut_infos.rpt {
+                    break;
+                };
+            }
+            Ok(60 * counter / (time.elapsed().as_millis() / 100))
         }
-        Err(e) => {
-            Err(e)
-        },
+        Err(e) => Err(e),
     }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -3,60 +3,68 @@ mod floodrs;
 #[macro_use(value_t)]
 extern crate clap;
 
-use clap::{App,Arg};
+use clap::{App, Arg};
 use std::path::Path;
 
-
-fn main() -> Result<(),image::ImageError> {
-
+fn main() -> Result<(), image::ImageError> {
     let arg = App::new("floodrs")
-            .version("0.1")
-            .about("Pixelflut rs client")
-            .author("buuky")
-            .arg(Arg::with_name("FILE")
+        .version("0.1")
+        .about("Pixelflut rs client")
+        .author("buuky")
+        .arg(
+            Arg::with_name("FILE")
                 .short("f")
                 .help("Path to Image")
                 .required(true)
-                .index(1))
-            .arg(Arg::with_name("IP")
+                .index(1),
+        )
+        .arg(
+            Arg::with_name("IP")
                 .short("ip")
                 .help("Server IP")
                 .required(false)
-                .index(2))
-            .arg(Arg::with_name("PORT")
+                .index(2),
+        )
+        .arg(
+            Arg::with_name("PORT")
                 .short("p")
                 .help("Server PORT")
                 .required(false)
-                .index(3))
-            .arg(Arg::with_name("xOffset")
+                .index(3),
+        )
+        .arg(
+            Arg::with_name("xOffset")
                 .short("x")
                 .help("X Offset")
                 .required(false)
-                .index(4))
-            .arg(Arg::with_name("yOffset")
+                .index(4),
+        )
+        .arg(
+            Arg::with_name("yOffset")
                 .short("y")
                 .help("y Offset")
                 .required(false)
-                .index(5))
-            .arg(Arg::with_name("repeat")
+                .index(5),
+        )
+        .arg(
+            Arg::with_name("repeat")
                 .short("r")
                 .help("redraw infinitely")
                 .required(false)
                 .min_values(0)
-                .index(6))
-            .get_matches();
+                .index(6),
+        )
+        .get_matches();
 
     let pixelflut_infos = floodrs::Config::new(
         arg.value_of("IP").unwrap_or("127.0.0.1"),
-        value_t!(arg.value_of("PORT"),u16).unwrap_or(1234u16),
+        value_t!(arg.value_of("PORT"), u16).unwrap_or(1234u16),
         floodrs::open_image(Path::new(arg.value_of("FILE").unwrap()))?,
-        value_t!(arg.value_of("xOffset"),u32).unwrap_or(0u32),
-        value_t!(arg.value_of("yOffset"),u32).unwrap_or(0u32),
+        value_t!(arg.value_of("xOffset"), u32).unwrap_or(0u32),
+        value_t!(arg.value_of("yOffset"), u32).unwrap_or(0u32),
         arg.is_present("repeat"),
     );
 
     floodrs::run(pixelflut_infos)?;
     Ok(())
 }
-
-
